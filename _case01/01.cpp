36673ffac9c21ec1c0fb7c5e9f3b7bc3443d377d
cypher <13104819533@163.com>
2025-10-21 14:06:57 +0800

    配置基本运行环境和新建文件夹、初始化一个 cpp 文件

---
+++ _case01/01.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+const int N = 1e5 + 10;
+int a[N], n;
+int main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr);
+
+    return 0;
+}
