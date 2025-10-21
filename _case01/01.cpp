1c6a31a3cbb0c692281183f9232742becad7a472
cypher <13104819533@163.com>
2025-10-21 14:23:25 +0800

    新增：输入数据规模和数值

--- _case01/01.cpp
+++ _case01/01.cpp
@@ -5,6 +5,8 @@ const int N = 1e5 + 10;
 int a[N], n;
 int main() {
     ios::sync_with_stdio(false), cin.tie(nullptr);
-
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
     return 0;
 }
